e010da8a7683939d157e3ace2df9d0b83752984f
recoilme <vadim-kulibaba@yandex.ru>
2021-02-11 13:08:32 +0300

    MainPageNoCache

--- api/index.go
+++ api/index.go
@@ -6,6 +6,8 @@ import (
 	"github.com/recoilme/imgproxycache/imgcache"
 )
 
+// Handler for vercel.com
+// test with: https://imgproxycache.vercel.app/api/index.go?url=https://i.trbna.com/preset/wysiwyg/2/61/0e0984cf811eb927ad03e1d88bea4.jpeg
 func Handler(w http.ResponseWriter, r *http.Request) {
-	imgcache.MainPage(w, r)
+	imgcache.MainPageNoCache(w, r)
 }
--- imgcache/handler.go
+++ imgcache/handler.go
@@ -50,3 +50,42 @@ func MainPage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+func MainPageNoCache(w http.ResponseWriter, r *http.Request) {
+	path := html.EscapeString(r.URL.Path)
+	log("mainPage", path)
+	switch r.Method {
+	case "GET":
+		vals := r.URL.Query()
+		url := vals.Get("url")
+		if path == "/" && url == "" {
+			w.WriteHeader(200)
+			return
+		}
+
+		bin, err := urlGet(url)
+		if err != nil {
+			log("urlGet err", err)
+			atomic.AddUint64(&reqError, 1)
+			w.WriteHeader(404)
+			return
+		}
+		buf := make([]byte, 512)
+		copy(buf, bin)
+		cntType := strings.ToLower(http.DetectContentType(buf))
+		if !strings.HasPrefix(cntType, "image") {
+			log("not image err", url)
+			w.WriteHeader(404)
+			atomic.AddUint64(&reqError, 1)
+			return
+		}
+		w.Header().Set("Content-Type", cntType)
+		atomic.AddUint64(&reqSuccess, 1)
+		w.Write(bin)
+		return
+	default:
+		log("wrong params")
+		w.WriteHeader(503)
+		return
+	}
+}
